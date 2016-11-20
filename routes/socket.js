c767275de4e93f3d4aa8c5aa68b40d07fa542c99
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2016-11-20 18:01:33 +0900

    socket update

--- routes/socket.js
+++ routes/socket.js
@@ -12,37 +12,93 @@ server.listen(8081);
 io.on('connection', function (socket) {
 	console.log('client connected');
 
-	socket.on('join', function (deviceId) {
+    // 라즈베리파이 접속
+	socket.on('raspberrypi-join', function (deviceId) {
 		// console.log('device ' + socket.name + ' connected');
-		socket.name = deviceId;
-		updateDeviceConnected(socket.name, 1);
+		socket.name = 'raspberrypi/' + deviceId;
+		updateDeviceConnected(deviceId, 1);
 	});
 
+    // 핸드폰 접속
+    socket.on('phone-join', function (userId) {
+        // console.log('device ' + socket.name + ' connected');
+        socket.name = 'phone/' + userId;
+
+    });
+
+    socket.on('phone-socket', function (deviceId) {
+        var intervalId = setInterval(function(){
+            if(socket.connected) {
+                var deviceInfo = getDeviceInfoByDeviceId(deviceId);
+                socket.emit('DeviceInfo', deviceInfo);
+            }else {
+                clearInterval(intervalId);
+            }
+        }, 5000);
+    });
+
+    // 라즈베리파이 디바이스 정보 업데이트
 	socket.on('updateDeviceInfo', function (deviceInfo) {
 		updateDeviceInfo(deviceInfo);
 	});
 
+    // 접속 끊겼을 때
 	socket.on('disconnect', function(){
         // console.log( socket.name + ' has disconnected from the chat.' + socket.id);
-        updateDeviceConnected(socket.name, 0);
+        var deviceType = socket.name.split("/")[0];
+        var deviceId = socket.name.split("/")[1];
+        if(deviceType == 'raspberrypi') {
+            updateDeviceConnected(deviceId, 0);
+        }
     });
 
+    // 라즈베리파이 이미지 저장
     socket.on('image', function(data) {
-        var dir = './public/images/';
-        var buffer = data["buffer"];
-        // console.log('image received: ' + buffer);
         
-        fs.writeFile(dir + socket.name + '.png', buffer, 'binary', function(err){
-            if(err)
-                console.log(err);
-            else
-                console.log(socket.name + '.png image saved');
-        });
+        var buffer = data["buffer"];
+        var deviceId = socket.name.split("/")[1];
+        // console.log('image received: ' + buffer);
+        saveImage(deviceId, buffer);
+        
     });
+
+
 });
 
 module.exports = router;
 
+
+// 라즈베리파이에서 받아온 이미지 저장
+const getNewImageIdQUERY = ("SELECT IFNULL(MAX(imageId), 0)+1 as imageId FROM Images WHERE deviceId = ?");
+const insertImageQUERY = ("INSERT INTO Images (deviceId, imageId, date) VALUES (?, ?, NOW())");
+
+function saveImage(deviceId, buffer) {
+    const dir = './public/images/';
+    const queryParams = [deviceId];
+    //console.log(queryParams);
+
+    connection.query(getNewImageIdQUERY, queryParams, function(err, rows, fields) {
+        if (rows.length == 0) {
+            return;
+        }
+
+        var imageId = rows[0].imageId;
+        const queryParams2 = [deviceId, imageId];
+
+        connection.query(insertImageQUERY, queryParams2, function(err, rows, fields) {
+            if (err || rows.affectedRows == 0) {
+                console.log('failed');
+            } else {
+                fs.writeFile(dir + deviceId + '-' + imageId + '.png', buffer, 'binary', function(err){
+                    if(err)
+                        console.log(err);
+                    else
+                        console.log(deviceId + '-' + imageId + '.png image saved');
+                });
+            }
+        });
+    });
+}
 
 
 // 라즈베리파이에서 기기의 현재 상태 업데이트
@@ -87,4 +143,18 @@ function updateDeviceConnected(deviceId, connected) {
 
 
 
+// 기기id로 기기 정보 조회
+const getDeviceInfoByDeviceIdQUERY = ("SELECT deviceId, deviceName, temperature, humidity, light, waterHeight FROM Devices WHERE deviceId = ?");
+
+function getDeviceInfoByDeviceId(deviceId) {
+    const queryParams = [deviceId];
+    //console.log(queryParams);
+    connection.query(getDeviceInfoByDeviceIdQUERY, queryParams, function(err, rows, fields) {
+        if(err) {
+                throw err;
+        }
+        
+        return rows[0];
+    });
+}
 
